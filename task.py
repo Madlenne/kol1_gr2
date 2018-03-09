92344c59d0fdb584bc0e14c1ffb0eaba55d30c2c
Madlenne <33123587+Madlenne@users.noreply.github.com>
2018-03-09 17:48:58 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,22 +1,37 @@
-###Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-##The program should:
-# - print out current orientation
-# - applied tilt correction
-# - run in infinite loop
-# - until user breaks the loop
-#Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations"). 
-#With every simulation step the orentation should be corrected, applied and printed out.
-#The level of realism in simulation is of your choice, but more sophisticated solutions are better.
-#If you can thing of any other features, you can add them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#The whole repository MUST be a fork from https://github.com/mwmajew/kol1_gr2
-#Good Luck
+
+
+import random
+import sys
+
+class FlightSimulator:
+
+	def __init__(self, angle = 0.0):
+		self.angle = angle
+
+
+	def print_orientation(self):
+		print "Current orientation is: {}".format(self.angle)
+
+	def proper_angle(self):
+		return 45.0
+
+
+i=0
+
+while True:
+	
+	if input ("Do you want to end the loop? Type Y for yes or N for no: ") == 'Y': 
+		break
+		
+	else:	
+		angle1 = random.randint(0, 360)
+		plane = FlightSimulator(angle1)	
+		plane.print_orientation()
+
+		if plane.angle != plane.proper_angle:
+			plane.angle = plane.proper_angle
+
+		plane.print_orientation()	
+
+
+		i+=1
